7fcaade5a3227fb72477d322a1595a6ec5ac676f
wangchao <360327741@qq.com>
2017-07-23 10:51:37 +0800

    add astar test

--- test_matrixgraph.cpp
+++ test_matrixgraph.cpp
@@ -8,6 +8,13 @@ typedef Dijkstra<MG> G;
 
 void print_path(const std::pair<G::VertexList, G::distance_t>& p)
 {
+    Matrix m;
+    m.resize(ROW, COL);
+    m.fill_all(0);
+    for (auto pos : p.first) {
+        m.set(pos, 1);
+    }
+    m.print();
     qDebug() << "path: "<<QList<Pos>::fromStdList(p.first);
     qDebug() << "dist: " << p.second;
 }
@@ -31,30 +38,34 @@ void test_matrixgraph()
     m.resize(ROW, COL);
     m.set_all_has();
 
-    m.set_not({0, 1});
-    m.set_not({1, 1});
-    m.set_not({1, 2});
-    m.set_not({2, 1});
-    m.set_not({3, 1});
-//    m.print();
-
-//    m.set_not({3, 0});
+    m.set_not({1, 6});
+    m.set_not({2, 6});
+    m.set_not({2, 7});
+    m.set_not({3, 7});
+    m.set_not({4, 7});
+    m.set_not({5, 7});
+    m.set_not({6, 7});
+    m.set_not({6, 6});
+    m.set_not({6, 5});
+    m.print();
 
     MG g(m);
     qDebug() << G::max_distant;
-    auto p = G::dijkstra_shortest_path(g, Pos(0, 0), Pos(0, 2));
-    auto pall = G::dijkstra_shortest_path_all(g, Pos(0,0));
+    Pos start(4, 0);
+    Pos end(4, 9);
+    auto p = G::dijkstra_shortest_path(g, start, end);
+    auto pall = G::dijkstra_shortest_path_all(g, start);
 
     print_path(p);
     print_all(pall);
 
 
-    auto mp = G::shortest_path(g, Pos(0, 0), Pos(0, 2));
-    auto mpall = G::shortest_path_all(g, Pos(0,0));
+    auto mp = G::shortest_path(g, start, end);
+    auto mpall = G::shortest_path_all(g, start);
 
     print_path(mp);
     print_all(mpall);
 
-    auto ap = G::AStart_path(g, Pos(0, 0), Pos(0, 2));
+    auto ap = G::AStart_path(g, start, end);
     print_path(ap);
 }
